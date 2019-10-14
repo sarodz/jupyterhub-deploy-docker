9388b0a3f152e26e7531259b760e912cf0c5c032
Michael Pilosov <consistentbayes@gmail.com>
2019-10-14 23:39:25 +0000

    dark mode.

--- jupyterhub_config.py
+++ jupyterhub_config.py
@@ -49,7 +49,8 @@ class MyDockerSpawner(DockerSpawner):
     def start(self):
         if self.user.name in self.group_map:
             group_list = self.group_map[self.user.name]
-            # self.volumes[os.path.join(pwd,'jupyterlab_settings')] = {'bind': '/home/jovyan/.jupyter/lab/user-settings/@jupyterlab'}
+            self.volumes['%s/jupyterlab_settings'%(os.environ['HUB_LOC'])] = \
+                    {'bind': '/home/jovyan/.jupyter/lab/user-settings/@jupyterlab'}
             #self.volumes['/tmp/.X11-unix'] = { 'bind': '/tmp/.X11-unix'}
             # add team volume to volumes
             if self.user.name == 'mathematicalmichael':
